ef79e3ce4ba7ed3edfd5ac85162339ccfa8d2283
bwapes <benwaples@gmail.com>
2020-06-25 16:54:01 -0700

    created test and it passed! LFG FUNCTIONS

--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,19 @@
 // IMPORT MODULES under test here:
-// import example from '../example.js';
+import {doIt} from '../utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('the function should recieve 2 number inputs and an operator input (as a string) and return the sum of combining all arguments', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const arg1 = 1;
+    const arg2 = 2;
+    const arg3 = '+';
+    const expected = 3;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const actual = doIt(arg1, arg2, arg3)
 
     //Expect
     // Make assertions about what is expected versus the actual result
