84ebb1116c609abe233fd646dd2d7e36c453b267
bwapes <benwaples@gmail.com>
2020-06-25 17:19:40 -0700

    added 2 more tests to this

--- test/example.test.js
+++ test/example.test.js
@@ -10,12 +10,25 @@ test('the function should recieve 2 number inputs and an operator input (as a st
     const arg2 = 2;
     const arg3 = '+';
     const expected = 3;
+
+    const try2arg1 = 4;
+    const try2arg2 = 2;
+    const try2arg3 = '-';
+    const try2expected = 2;
+
+    const try3arg1 = 4;
+    const try3arg2 = 5;
+    const try3arg3 = '*';
+    const try3expected = 20;
     
     //Act 
     // Call the function you're testing and set the result to a const
     const actual = doIt(arg1, arg2, arg3)
-
+    const actual2 = doIt(try2arg1, try2arg2, try2arg3)
+    const actual3 = doIt(try3arg1, try3arg2, try3arg3)
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
+    expect.equal(actual2, try2expected)
+    expect.equal(actual3, try3expected)
 });
